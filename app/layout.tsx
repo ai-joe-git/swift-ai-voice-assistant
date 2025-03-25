061f02679591dec53acd9d46a3b8413981e70a85
A.I Joe <88169436+ai-joe-git@users.noreply.github.com>
2025-03-25 03:26:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
 	title: "Swift",
 	description:
-		"A fast, open-source voice assistant powered by Groq, Cartesia, and Vercel.",
+		"Talk with Donald Trump!.",
 };
 
 export default function RootLayout({
